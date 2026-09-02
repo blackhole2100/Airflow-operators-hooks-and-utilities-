110c174ff1d5af4b5469246f8f05c9661adc6191
blackhole2100 <kevin.o881129@gmail.com>
2026-09-02 08:04:14 -0700

    copyright

--- airflow_dbt_python/__version__.py
+++ airflow_dbt_python/__version__.py
@@ -1,6 +1,6 @@
 """The module's version information."""
 
-__author__ = "Tomás Farías Santana"
-__copyright__ = "Copyright 2021 Tomás Farías Santana"
+__author__ = "All right"
+__copyright__ = "Copyright 2021 All right"
 __title__ = "airflow-dbt-python"
 __version__ = "3.6.0"
